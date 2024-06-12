a7d7e3b0acce5742bc75bb786ed0f8505b1d7b65
Sina <stabeiy@gmail.com>
2024-06-12 16:05:58 -0400

    The kinematic data is now extracted!

---
+++ Project_001.py
@@ -0,0 +1,79 @@
+import pandas as pd
+import numpy as np
+import os
+from scipy.io import loadmat, whosmat
+import matplotlib.pyplot as plt
+
+
+# RREAD .mat    FILES IN PYTHON
+
+def access_struct (data,structs):
+    for struct in structs:
+        
+        if isinstance(data,np.ndarray) and data.dtype.names is not None:
+            data = data[0,0][struct]
+        else:
+            data = data[struct]
+        
+    return data
+
+
+
+
+"""
+
+def access_struct (data,structs):
+    for struct in structs:
+        
+        if isinstance(data,np.ndarray):
+            if data.dtype.names is not None:
+                data = data[struct]
+            else:
+                if data.ndim == 0:
+                    data = data.item()
+                else:
+                    data = data[struct]
+
+        elif isinstance(data,dict):
+            data = data[struct]
+                
+    return data
+
+"""
+
+# MAKE SURE YOU DO NOT SQUEEZE DATA BY  squeeze_me= True. OTHERWISE THE CODE RUNS INTO ERRORS
+file_path = r"D:\Sina Tabeiy\Clustering Project\Lokomat Data (matfiles)\patient1_PostLokomat.mat"
+data = loadmat(file_path)
+
+# WRITE THE NAME OF STRUCTS YOU WANT TO INCLUDE, DO NOT FORGER TO PUT THEM IN ORDERD
+structs = ['c', 'results', 'Right', 'angAtFullCycle']
+data = access_struct(data,structs)
+
+
+
+kin_items = ['Hip', 'Knee', 'Ankle', 'FootProgress', 'Thorax', 'Pelvis']
+#kin_items = ['Hip']
+
+sides = ['L', 'R']
+
+joint_data = np.empty((100,0))
+#list_joint_kin = []
+
+for joint in (kin_items):
+    for side in sides:
+
+        joint_with_side = side + joint
+        joint_kin = data[0,0][joint_with_side][0][0]
+        #list_joint_kin = joint_kin.flatten(order = "F")
+        joint_kin = np.reshape(joint_kin, (100,3), order = 'F')
+        #joint_kin1 = [item for sublist in list_joint_kin for item in sublist]
+        #df.append(joint_with_side)
+        
+        #joint_data.append(joint_kin)
+        joint_data = np.append(joint_data,joint_kin, axis = 1)
+
+
+#print(joint_data)
+
+pd.DataFrame(joint_data).to_csv('output1.csv')
+    
