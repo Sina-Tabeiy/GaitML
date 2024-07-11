ff326279eee7cb434072214c8879be7542f0b843
Sina <stabeiy@gmail.com>
2024-07-11 15:57:03 -0400

    New files added!

--- Classification/SVM_Classification_v0.4.py
+++ Classification/SVM_Classification_v0.4.py
@@ -30,26 +30,33 @@ file_directory = r'D:\Sina Tabeiy\Project\Lokomat Data (matfiles)\Pre Data'
 # measurements = ['pctToeOff', 'pctSimpleAppuie', 'distPas', 'distFoulee', 'tempsFoulee']
 measurements = ['pctToeOff', 'pctSimpleAppuie', 'distPas', 'distFoulee', 'tempsFoulee']
 #joint_names = ['Hip', 'Knee', 'Ankle', 'FootProgress', 'Thorax', 'Pelvis']
+side = ['Right', 'Left']
 output_dir = r'D:\Sina Tabeiy\Project\Lokomat Data (matfiles)\CSV Output'
 significance_value = -1
 
-# ----------
+# ----- Extract variables from .mat files -----
 # The output data will be in the order of all pre intervention files and then all post intervention files. 
-# Since the folder only contain Pre data so we are gonna have the pre data only. 
+# Since the folder only contains Pre data so we are gonna have the pre data only. 
 all_data = featurextractor.feature_extractor(file_directory, measurements, output_dir, separate_legs = False)
+if isinstance(all_data, pd.DataFrame):
+    all_data = all_data.values
 
+# ----- Add participants demographic variables -----
+demo_var = pd.read_excel(r'D:\Sina Tabeiy\Project\Lokomat Data (matfiles)\Pre Data\Participants_caracteristic_ML.xlsx')
+demo_var.drop(['Patient', 'sex','masse', 'taille', 'Diagnostique'], axis = 1, inplace= True)
+# demo_var.replace(['Quadri', 'Double Hémi', 'Diplégie'], [3,2,1], inplace=True)
+demo_var.replace(['walker', 'cane', 'none'], [3,2,1], inplace=True)
+demo_val = demo_var.values
+all_data = np.concatenate((all_data, demo_val), axis=1)
 
 # ----- Load the final result and label the data -----
-gps = pd.read_csv(r'D:\Sina Tabeiy\Project\Results\GPS_results\GPS_output.csv')
+gps = pd.read_csv(r'D:\Sina Tabeiy\Project\Results\GPS_results\GPS_output_xp5.csv')
 diffrence = np.diff(gps,axis=1)
 labels = np.where(diffrence < significance_value, 1, 0).flatten()
 # ACTION REQUIRED: IF RUNNING WITH separate_legs = False, DEACTIVATE THE FOLLOWING LINE. OTHERWISE, KEEP IT ACTIVATED.
-# labels = np.repeat(labels, 2)
+#labels = np.repeat(labels, 2)
 
-if isinstance(all_data, pd.DataFrame):
-    all_data = all_data.values
 # --------------- ML algorithm: Support Vector Machine (SVM) ---------------
-
 start_time = time.time()
 
 # ----- Train / test Split -----
@@ -66,7 +73,7 @@ x_test = scaler.transform(x_test)
 print('************************************')
 print('Grid Search initiated.')
 
-SVM = svm.SVC(kernel = 'rbf', random_state = 0)
+SVM = svm.SVC(kernel = 'rbf',random_state = 0)
 
 parameter_grid = {
     'C': [1, 10, 100, 1000],
@@ -77,7 +84,7 @@ parameter_grid = {
 }
 
 # kfold = KFold(n_splits = 5, shuffle = True, random_state = 0)
-grid_search = GridSearchCV(SVM, parameter_grid, n_jobs = -1, refit = True, verbose = 3, cv = 5)
+grid_search = GridSearchCV(SVM, parameter_grid, n_jobs = -1, refit = True, verbose = 3)
 grid_search.fit(x_train, y_train)
 
 print('********** Best parameters ********** ')
@@ -86,14 +93,16 @@ print('********** Best estimator ********** ')
 print("The best estimator is:", grid_search.best_estimator_)
 print("Test set score: ", grid_search.score(x_test, y_test))
 #print(grid_search.best_score_)
-print(metrics.classification_report(y_test, grid_search.best_estimator_(x_test)))
+print(metrics.classification_report(y_test, grid_search.best_estimator_.predict(x_test)))
 
 # ----- Calculate parameters weight fi=or non-linear model -----
 fw = permutation_importance(grid_search.best_estimator_, x_test, y_test, n_repeats = 20, n_jobs=-1, random_state = 0)
 for i in range(len(fw.importances_mean)):
         print(f"{fw.importances_mean[i]:.3f} +/- {fw.importances_std[i]:.3f}")
 
-plt.barh(measurements, fw.importances_mean)
+feature = [s[0] + m for s in side for m in measurements]
+feature = feature + list(demo_var.columns)
+plt.barh(feature, fw.importances_mean)
 plt.xlabel("Permutation Importance")
 plt.show()
 
@@ -101,7 +110,11 @@ plt.show()
 # ----- Calculate patameters weight for linear model -----
 linear_svm = svm.SVC(kernel = 'linear', random_state = 0)
 clf = linear_svm.fit(x_train, y_train)
-print(clf.coef_)
+print('Weight of the Linear model: ', clf.coef_)
+plt.barh(feature, abs(clf.coef_[0]))
+plt.xlabel("Feature weight for Linear model")
+plt.show()
+print('accuracy of the Linear model: ', metrics.accuracy_score(y_test,clf.predict(x_test)))
 
 end_time = time.time()
 print("The run time of the code is %f seconds" %(end_time - start_time))
